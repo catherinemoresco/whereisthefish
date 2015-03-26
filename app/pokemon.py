7c9df0110b28ff645de2cf6ce2304604e5f407c6
Patrick Facheris <plfacheris@gmail.com>
2015-03-26 01:23:44 -0400

    fully working new version

--- app/pokemon.py
+++ app/pokemon.py
@@ -23,11 +23,13 @@ class Pokemon:
   def __init__(self):
       self.remove()
 
-  def change(self, code, nickname=None):
+  def change(self, code):
+      if self.code == code:
+          return
       try:
           self.name = [''] * 10
-          temp_name = list(pokedex_code_map[code]["name"]) if not nickname else list(nickname)
-          self.name[0:len(name) - 1] = temp_name[:]
+          temp_name = list(pokedex_code_map[code]["name"])
+          self.name[0:len(temp_name) - 1] = temp_name
       except:
           self.remove()
       else:
@@ -35,11 +37,18 @@ class Pokemon:
           self.level = 1
 
   def remove(self):
-      self.name = ['E', 'm', 'p', 't', 'y', '', '', '', '', '']
+      self.name = [''] * 10
       self.code = None
       self.level = None
 
-roster = [Pokemon()] * 6
+  def formatted_name(self):
+      if self.code == None:
+          return "Empty"
+      else:
+          return "".join(self.name)
+
+
+roster = [Pokemon() for i in xrange(6)]
 
 def decode_char(val):
     if val in xrange(0x80, 0x99 + 1):
@@ -66,32 +75,41 @@ def monitor_team(pipe):
         else:
             if val == 0xff:
                 continue
-            elif address in roster_code_locations:
-                roster[roster_code_locations.index(address)].change(val)
+            if address in roster_code_locations:
+                roster[roster_code_locations.index(address)].change(line[1].upper())
             elif address in roster_level_locations:
                 roster[roster_level_locations.index(address)].level = val
             elif address in xrange(roster_nickname_locations[0][0], roster_nickname_locations[-1][-1] + 1):
                 full_index = range(roster_nickname_locations[0][0], roster_nickname_locations[-1][-1] + 1).index(address)
                 nickname_index = full_index / 11
-                char_index = full_index % 11 - 1
+                char_index = full_index % 11
+                if char_index == 10:
+                    continue
                 roster[nickname_index].name[char_index] = decode_char(val)
-
         time.sleep(0)
+
+def draw_centered_text(draw, msg, font, container_width, container_corner):
+    w, h = draw.textsize(msg, font=font)
+    centered_corner = copy(container_corner)
+    centered_corner[0] += (container_width - w) / 2
+    draw.text(centered_corner, msg, font=font)
 
 def render_team(image, draw, font):
     for index, pokemon in enumerate(roster):
         # draw name
-        msg = "".join(pokemon.name)
-        corner = [__BORDER_WIDTH * (index + 1) + __BOX_WIDTH * index, 490]
+        msg = pokemon.formatted_name()
+        corner = [__BORDER_WIDTH * (index + 1) + __BOX_WIDTH * index, 495]
         if index > 2:
             corner[0] += 302
-        w, h = draw.textsize(msg, font=font)
-        centered_corner = copy(corner)
-        centered_corner[0] += (__BOX_WIDTH - w) / 2
-        draw.text(centered_corner, msg, font=font)
+        draw_centered_text(draw, msg, font, __BOX_WIDTH, corner)
 
-        # draw image
         if pokemon.code != None:
+            # draw level
+            msg = "Lv" + str(pokemon.level)
+            corner[1] = 515
+            draw_centered_text(draw, msg, font, __BOX_WIDTH, corner)
+
+            # draw image
             sprite = Image.open(abspath(join(image_path + "/sprites", pokedex_code_map[pokemon.code]["id"].lstrip("0") + ".png")))
             corner[1] = 549
             w, h = sprite.size
