f5707e90b965e4260407142941bca5fe8aaeca0e
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-23 18:41:50 +0800

    Delete environment directory

--- environment/pulseaudio.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec /usr/bin/pulseaudio >>/var/log/pulseaudio.log 2>&1
--- environment/xvfb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-exec /usr/bin/Xvfb :0 -screen 0 480x432x24 -ac +extension GLX +render -noreset >>/var/log/xvfb.log 2>&1
